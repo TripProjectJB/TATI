a9f66e2895a1809ae0f3ca97f68b28dbc246de09
jeheon0717 <jeheon0717@naver.com>
2023-11-03 14:14:58 +0900

    <FEAT(MemberService)> : 회원 서비스 인터페이스, 함수 구현

--- EnjoyTrip_Framework_seoul_15/src/main/java/com/ssafy/member/model/service/MemberServiceImpl.java
+++ EnjoyTrip_Framework_seoul_15/src/main/java/com/ssafy/member/model/service/MemberServiceImpl.java
@@ -1,66 +1,62 @@
 package com.ssafy.member.model.service;
 
-import com.ssafy.member.model.MemberDto;
-import com.ssafy.member.model.mapper.MemberDao;
-import com.ssafy.member.model.mapper.MemberDaoImpl;
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ssafy.member.model.MemberDto;
+import com.ssafy.member.model.mapper.MemberMapper;
+
+@Service
 public class MemberServiceImpl implements MemberService {
 	
-	private static MemberService memberService = new MemberServiceImpl();
-	private MemberDao memberDao;
-	
-	private MemberServiceImpl() {
-		memberDao = MemberDaoImpl.getMemberDao();
-	}
-	
-	public static MemberService getMemberService() {
-		return memberService;
+	private MemberMapper memberMapper;
+
+	@Autowired
+	public MemberServiceImpl(MemberMapper memberMapper) {
+		super();
+		this.memberMapper = memberMapper;
 	}
 
 	@Override
 	public int idCheck(String userId) throws Exception {
-		return memberDao.idCheck(userId);
+//		return sqlSession.getMapper(MemberMapper.class).idCheck(userId);
+		return memberMapper.idCheck(userId);
 	}
 
 	@Override
-	public int joinMember(MemberDto memberDto) throws Exception {
-		//비밀번호 해쉬화
-		String hashPwd = hashPassword(memberDto.getUserPwd());
-		memberDto.setUserPwd(hashPwd);
-		return memberDao.joinMember(memberDto);
+	public void joinMember(MemberDto memberDto) throws Exception {
+//		sqlSession.getMapper(MemberMapper.class).joinMember(memberDto);
+		memberMapper.joinMember(memberDto);
 	}
 
 	@Override
-	public MemberDto loginMember(String userId, String userPwd) throws Exception {
-		//비밀번호 해쉬화
-		String hashPwd = hashPassword(userPwd);
-		return memberDao.loginMember(userId, hashPwd);
+	public MemberDto loginMember(Map<String, String> map) throws Exception {
+//		return sqlSession.getMapper(MemberMapper.class).loginMember(map);
+		return memberMapper.loginMember(map);
+	}
+	
+	/* ADMIN */
+	@Override
+	public List<MemberDto> listMember(Map<String, Object> map) throws Exception {
+		return memberMapper.listMember(map);
 	}
 
 	@Override
-	public int modifyMember(MemberDto member) throws Exception {
-		String hashPwd = hashPassword(member.getUserPwd());
-		member.setUserPwd(hashPwd);
-		return memberDao.modifyMember(member);
+	public MemberDto getMember(String userId) throws Exception {
+		return memberMapper.getMember(userId);
 	}
 
 	@Override
-	public int deleteMember(String userId) throws Exception {
-		return memberDao.deleteMember(userId);
+	public void updateMember(MemberDto memberDto) throws Exception {
+		memberMapper.updateMember(memberDto);
 	}
 
 	@Override
-	public String hashPassword(String userPwd) throws Exception {
-		int key = 0717;
-		StringBuilder sb = new StringBuilder();
-		char[] pwd = userPwd.toCharArray();
-		int len = userPwd.length();
-		for (int i = 0; i < len; i++) {
-			char c = (char) ((pwd[i] * len * key + 33) % 127);
-			if (33 > c) c+=33;
-			System.out.println((int)c + " " + c);
-			sb.append(c);
-		}
-		return sb.toString();
+	public void deleteMember(String userId) throws Exception {
+		memberMapper.deleteMember(userId);		
 	}
+
 }
