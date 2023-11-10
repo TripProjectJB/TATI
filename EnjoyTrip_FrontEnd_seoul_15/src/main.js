1d52a09ae2b58950cf0c880b0d8e045366b981f8
= <=>
2023-11-10 13:26:35 +0900

    '컴포넌트화'

--- EnjoyTrip_FrontEnd_seoul_15/src/main.js
+++ EnjoyTrip_FrontEnd_seoul_15/src/main.js
@@ -1,5 +1,3 @@
-import "./assets/main.css";
-
 import {createApp} from "vue";
 // import { createPinia } from 'pinia'
 
@@ -7,12 +5,10 @@ import App from "./App.vue";
 import router from "./router";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
-import Axios from "axios";
 
 const app = createApp(App);
 
 // app.use(createPinia())
 app.use(router);
-app.use(Axios);
 
 app.mount("#app");
---
+++ EnjoyTrip_FrontEnd_seoul_15/src/views/MainView.vue
@@ -0,0 +1,33 @@
+<script setup>
+import TheHeading from "../components/layout/TheHeading.vue";
+import VBanner from "../components/common/VBanner.vue";
+import VSectionList from "../components/common/VSectionList.vue";
+import VSectionCard from "../components/common/VSectionCard.vue";
+import TheAsideNav from "../components/layout/TheAsideNav.vue";
+</script>
+
+<template>
+    <div id="wrapper">
+        <!-- Main -->
+        <div id="main">
+            <div class="inner">
+                <!-- Header -->
+                <TheHeading />
+
+                <!-- Banner -->
+                <VBanner />
+
+                <!-- Section -->
+                <VSectionList />
+
+                <!-- Section -->
+                <VSectionCard />
+            </div>
+        </div>
+
+        <!-- Sidebar -->
+        <TheAsideNav />
+    </div>
+</template>
+
+<style scoped></style>
