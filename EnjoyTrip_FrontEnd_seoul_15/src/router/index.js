218e89ef494947a471ad922bd929f6444913784b
jeghtner <jeheon0717@naver.com>
2023-11-12 17:54:34 +0900

    map

--- EnjoyTrip_FrontEnd_seoul_15/src/router/index.js
+++ EnjoyTrip_FrontEnd_seoul_15/src/router/index.js
@@ -1,21 +1,27 @@
-import {createRouter, createWebHistory} from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import MainView from "../views/MainView.vue";
 import ElementsView from "../views/ElementsView.vue";
+import MapView from "../views/MapView.vue";
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: "/",
-            name: "main",
-            component: MainView,
-        },
-        {
-            path: "/elements",
-            name: "elements",
-            component: ElementsView,
-        },
-    ],
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "main",
+      component: MainView,
+    },
+    {
+      path: "/elements",
+      name: "elements",
+      component: ElementsView,
+    },
+    {
+      path: "/map",
+      name: "map",
+      component: MapView,
+    },
+  ],
 });
 
 export default router;
