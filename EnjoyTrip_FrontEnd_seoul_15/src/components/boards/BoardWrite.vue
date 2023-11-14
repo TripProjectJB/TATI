da3807fbe57a777d26a847dcad245131894ac3bf
= <=>
2023-11-14 16:46:41 +0900

    FEAT:게시판 CRUD구현

---
+++ EnjoyTrip_FrontEnd_seoul_15/src/components/boards/BoardWrite.vue
@@ -0,0 +1,101 @@
+<script setup>
+import {ref} from "vue";
+import {useRouter, useRoute} from "vue-router";
+import {registArticle} from "@/api/board";
+
+const router = useRouter();
+
+const article = ref({
+    subject: "",
+    content: "",
+    userId: "",
+    userName: "",
+    hit: 0,
+    registerTime: "",
+});
+
+function writeArticle() {
+    registArticle(
+        article.value,
+        (response) => {
+            let msg = "글등록 처리시 문제 발생했습니다.";
+            if (response.status == 201) msg = "글등록이 완료되었습니다.";
+            alert(msg);
+            router.push({name: "list"});
+        },
+        (error) => console.log(error)
+    );
+}
+</script>
+
+<template>
+    <section class="content">
+        <header>
+            <h1>Type A Board</h1>
+            <h3>Write</h3>
+        </header>
+        <form method="post" action="#">
+            <div class="row uniform">
+                <div class="6u 12u$(xsmall)">
+                    <input type="text" name="demo-email" id="demo-email" placeholder="Id" v-model="article.userId" />
+                </div>
+                <div class="6u$ 12u$(xsmall)">
+                    <input type="text" name="demo-name" id="demo-name" placeholder="Name" v-model="article.userName" />
+                </div>
+
+                <!-- Break -->
+                <div class="3u 6u(small)">
+                    <input type="radio" id="demo-A" name="demo-priority" checked />
+                    <label for="demo-A">A</label>
+                </div>
+                <div class="3u 6u$(small)">
+                    <input type="radio" id="demo-B" name="demo-priority" />
+                    <label for="demo-B">B</label>
+                </div>
+                <div class="3u 6u(small)">
+                    <input type="radio" id="demo-C" name="demo-priority" />
+                    <label for="demo-C">C</label>
+                </div>
+                <div class="3u$ 6u$(small)">
+                    <input type="radio" id="demo-D" name="demo-priority" />
+                    <label for="demo-D">D</label>
+                </div>
+                <!-- Break -->
+                <!-- <div class="6u 12u$(small)">
+                    <input type="checkbox" id="demo-copy" name="demo-copy" />
+                    <label for="demo-copy">Email me a copy</label>
+                </div>
+                <div class="6u$ 12u$(small)">
+                    <input type="checkbox" id="demo-human" name="demo-human" checked />
+                    <label for="demo-human">I am a human</label>
+                </div> -->
+                <!-- Break -->
+                <div class="12u$">
+                    <input type="text" name="demo-name" id="demo-name" placeholder="제목" v-model="article.subject" />
+                </div>
+                <!-- Break -->
+                <div class="12u$">
+                    <textarea
+                        name="demo-message"
+                        id="demo-message"
+                        placeholder="내용"
+                        rows="6"
+                        v-model="article.content"></textarea>
+                </div>
+                <!-- Break -->
+                <div class="12u$">
+                    <ul class="actions">
+                        <li>
+                            <span class="button" @click="writeArticle">Write</span>
+                        </li>
+                        <li>
+                            <span class="button special" @click="$router.push({name: 'list'})">List</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </form>
+    </section>
+</template>
+
+<style scoped></style>
--- EnjoyTrip_FrontEnd_seoul_15/src/router/index.js
+++ EnjoyTrip_FrontEnd_seoul_15/src/router/index.js
@@ -1,41 +1,59 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {createRouter, createWebHistory} from "vue-router";
 import MainView from "../views/MainView.vue";
 import ElementsView from "../views/ElementsView.vue";
 import MapView from "../views/MapView.vue";
-import BoardView from "../views/BoardView.vue";
-import BoardList from "../components/board/BoardList.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "main",
-      component: MainView,
-    },
-    {
-      path: "/elements",
-      name: "elements",
-      component: ElementsView,
-    },
-    {
-      path: "/map",
-      name: "map",
-      component: MapView,
-    },
-    {
-      path: "/board",
-      name: "board",
-      component: BoardView,
-      children: [
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
         {
-          path: "list",
-          name: "list",
-          component: BoardList,
+            path: "/",
+            name: "main",
+            component: MainView,
         },
-      ]
-    },
-  ],
+        {
+            path: "/elements",
+            name: "elements",
+            component: ElementsView,
+        },
+        {
+            path: "/map",
+            name: "map",
+            component: MapView,
+        },
+        {
+            path: "/board",
+            name: "board",
+            // component: TheBoardView,
+            // route level code-splitting
+            // this generates a separate chunk (About.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import("../views/BoardView.vue"),
+            redirect: {name: "list"},
+            children: [
+                {
+                    path: "list",
+                    name: "list",
+                    component: () => import("@/components/boards/BoardList.vue"),
+                },
+                {
+                    path: "detail/:articleno",
+                    name: "detail",
+                    component: () => import("@/components/boards/BoardDetail.vue"),
+                },
+                {
+                    path: "write",
+                    name: "write",
+                    component: () => import("@/components/boards/BoardWrite.vue"),
+                },
+                {
+                    path: "modify/:articleno",
+                    name: "modify",
+                    component: () => import("@/components/boards/BoardModify.vue"),
+                },
+            ],
+        },
+    ],
 });
 
 export default router;
