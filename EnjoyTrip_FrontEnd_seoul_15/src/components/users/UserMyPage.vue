4b93f17b0d5bd2ed7d59251daf7f80d361a942ae
jeheon0717 <jeheon0717@naver.com>
2023-11-16 12:33:14 +0900

    FEAT : 마이페이지 기능 구현

--- EnjoyTrip_FrontEnd_seoul_15/src/components/users/UserMyPage.vue
+++ EnjoyTrip_FrontEnd_seoul_15/src/components/users/UserMyPage.vue
@@ -5,10 +5,12 @@
 		<section class="section">
 			<div class="container mt-3">
 				<div class="col col-lg-3 offset-lg-2">
-					<h3 class="mx-3">내 정보</h3>
+					<h1 class="mx-3" style="display: flex; justify-content: center; margin: 30px 0">
+						내 정보
+					</h1>
 				</div>
-				<div class="row justify-content-center">
-					<!-- <div class="col-lg-3 text-center border border-dark mx-auto my-auto" ></div> -->
+				<div class="table-wrapper">
+					<!-- <div class="col-lg-3 text-center border border-dark mx-auto my-auto"></div> -->
 					<div class="col-lg-9 text-center border border-dark mx-auto my-auto">
 						<table class="table">
 							<tbody>
@@ -60,46 +62,19 @@
 					<div class="row justify-content-center">
 						<div class="col col-lg-3 text-center">
 							<router-link to="/mypage/modify">
-								<MaterialButton class="my-4 mb-2" variant="gradient" color="dark" fullWidth
-									>회원 정보 수정</MaterialButton
+								<Button class="my-4 mb-2" variant="gradient" color="dark" fullWidth
+									>회원 정보 수정</Button
 								>
 							</router-link>
 						</div>
 						<div class="col col-lg-3 text-center">
 							<router-link to="/mypage/delete">
-								<MaterialButton class="my-4 mb-2" variant="gradient" color="danger" fullWidth
-									>회원 탈퇴</MaterialButton
+								<Button class="my-4 mb-2" variant="gradient" color="danger" fullWidth
+									>회원 탈퇴</Button
 								>
 							</router-link>
 						</div>
 					</div>
-				</div>
-			</div>
-		</section>
-		<section class="section">
-			<div class="container mt-3">
-				<div class="row justify-content-center">
-					<div class="text-center mt-5">
-						<h3>내가 좋아요한 핫플레이스 목록</h3>
-						<div class="mt-5">
-							<!-- <div class="row" v-if="hstore.recList != null">
-								<HotPlaceDetail
-									v-for="(article, index) in hstore.recList"
-									:key="article.hotplaceNo"
-									:article="article"
-									:index="index"
-									:backend_base_url="backend_base_url"
-									:user-id="ustore.userInfo.id"></HotPlaceDetail>
-							</div>
-							<div class="col-lg-6 offset-lg-2" v-if="hstore.recList == null">
-								아직 좋아요한 핫플레이스가 없습니다...
-							</div> -->
-						</div>
-					</div>
-					<!-- <div class="col-lg-6 mt-5 text-center">
-            <h3>내가 추천한 여행 정보 공유글</h3>
-            <div class="border border-dark"></div>
-          </div> -->
 				</div>
 			</div>
 		</section>
