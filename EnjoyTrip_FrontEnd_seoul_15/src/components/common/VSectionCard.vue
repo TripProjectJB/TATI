1bedcdee9dd20c23705b2d50d1b9ff346db68919
= <=>
2023-11-20 08:31:09 +0900

    dsd

--- EnjoyTrip_FrontEnd_seoul_15/src/components/common/VSectionCard.vue
+++ EnjoyTrip_FrontEnd_seoul_15/src/components/common/VSectionCard.vue
@@ -9,8 +9,6 @@ const {getAttractions} = store;
 const {articles} = storeToRefs(store);
 onMounted(async () => {
     await getAttractions();
-    console.log(articles);
-    console.log(articles.value[0]);
 });
 </script>
 
