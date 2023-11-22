2f38c29c3b047c2ab52fb98ca11b182082614ecb
jeghtner <jeheon0717@naver.com>
2023-11-22 19:39:09 +0900

    FIX : 친구 좋아요여행지 정상출력

--- EnjoyTrip_FrontEnd_seoul_15/src/components/users/UserAttractionLikeList.vue
+++ EnjoyTrip_FrontEnd_seoul_15/src/components/users/UserAttractionLikeList.vue
@@ -25,70 +25,80 @@ const temp = ref([]);
 const listLength = ref(0);
 
 const thisUserGetLikeList = async (userId) => {
-	await likeList(
-		userId,
-		(response) => {
-			if (response.status == httpStatusCode.OK) {
-				temp.value = response.data;
-			} else {
-				console.log("좋아요 리스트 실패");
-			}
-		},
-		(error) => {}
-	);
+  console.log("thisUserGetLikeList");
+  await likeList(
+    userId,
+    (response) => {
+      console.log("response: ", response);
+      if (response.status == "200") {
+        temp.value = response.data;
+        console.log("temp.value: ", temp.value);
+        console.log("좋아요 리스트 성공");
+      } else {
+        console.log("좋아요 리스트 실패");
+      }
+    },
+    (error) => {}
+  );
 };
 
 const getThisUsersAttractionLikeList = async (id) => {
-	await thisUserGetLikeList(id);
-	const attractionDetailsPromises = temp.value.map(async (contentId) => {
-		const attraction = ref({});
-		await attStore.getAttractionOtherDetail(contentId, attraction);
-		console.log("attraction: ", attraction.value);
-		return attraction.value;
-	});
-	console.log("attractionDetailsPromises: ", attractionDetailsPromises);
-	const attractionDetails = await Promise.all(attractionDetailsPromises);
+  await thisUserGetLikeList(id);
+  const attractionDetailsPromises = temp.value.map(async (contentId) => {
+    const otherAttraction = ref({});
+    await attStore.getAttractionOtherDetail(contentId, otherAttraction);
+    console.log("attraction: ", otherAttraction.value);
+    return otherAttraction.value;
+  });
+  console.log("attractionDetailsPromises: ", attractionDetailsPromises);
+  const attractionDetails = await Promise.all(attractionDetailsPromises);
 
-	thisUsersAttractionLikeList.value = attractionDetails;
-	// console.log(thisUsersAttractionLikeList.value);
-	listLength.value = thisUsersAttractionLikeList.value.length;
+  thisUsersAttractionLikeList.value = attractionDetails;
+  // console.log(thisUsersAttractionLikeList.value);
+  listLength.value = thisUsersAttractionLikeList.value.length;
 };
 getThisUsersAttractionLikeList(route.params.userid);
 </script>
 
 <template>
-	<header class="main">
-		<p>&nbsp;</p>
-		<h1 class="icon fa-lightbulb-o" style="text-align: center">
-			&nbsp; {{ route.params.userid }}의 좋아요 리스트 : {{ listLength }}
-		</h1>
-		<p>&nbsp;</p>
-	</header>
+  <header class="main">
+    <p>&nbsp;</p>
+    <h1 class="icon fa-lightbulb-o" style="text-align: center">
+      &nbsp; {{ route.params.userid }}의 좋아요 리스트 : {{ listLength }}
+    </h1>
+    <p>&nbsp;</p>
+  </header>
 
-	<div class="row" style="overflow: scroll; width: auto; height: 1000px">
-		<table class="table table-striped mt-3">
-			<thead style="font-size: large">
-				<tr style="text-align: center">
-					<th>대표이미지</th>
-					<th>관광지명</th>
-					<th>주소</th>
-					<th>좋아요</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr
-					v-for="(area, index) in thisUsersAttractionLikeList"
-					:key="area.content_id"
-					class="trip-info view"
-					@click="$router.push({ name: 'map-detail', params: { id: area.content_id } })">
-					<td><img :src="area.first_image || altImage" :width="100" /></td>
-					<td>{{ area.title }}</td>
-					<td>{{ area.addr1 }} {{ area.addr2 }}</td>
-					<td>{{ area.mlevel }}</td>
-				</tr>
-			</tbody>
-		</table>
-	</div>
+  <div class="row" style="overflow: scroll; width: auto; height: 1000px">
+    <table class="table table-striped mt-3">
+      <thead style="font-size: large">
+        <tr style="text-align: center">
+          <th>대표이미지</th>
+          <th>관광지명</th>
+          <th>주소</th>
+          <th>좋아요</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="(area, index) in thisUsersAttractionLikeList"
+          :key="area.content_id"
+          class="trip-info view"
+          @click="
+            $router.push({
+              name: 'map-detail',
+              params: { id: area.content_id },
+            })
+          "
+        >
+          <td><img :src="area.first_image || altImage" :width="100" /></td>
+          <td>{{ area.title }}</td>
+          <td>{{ area.addr1 }} {{ area.addr2 }}</td>
+          <td>{{ area.mlevel }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <style scoped></style>
